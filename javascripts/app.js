6f74823145709423c126a2dfd5c5f64f17e94d70
Julie Kraft <julieschmoo@gmail.com>
2014-02-21 14:58:09 -0500

    colors from first letter of word

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,34 @@
 
 // var word_array = []
 
+var colors = {a: '(255, 220, 36)', 
+              b: '(193, 0, 7)',
+              c: '(27, 177, 0)',
+              d: '(0, 78, 25)',
+              e: '(252, 88, 0)',
+              f: '(116, 69, 37)',
+              g: '(82, 85, 133)',
+              h: '(209, 84, 133)',
+              i: '(224, 224, 224)',
+              j: '(98, 81, 66)',
+              k: '(111, 46, 0)',
+              l: '(49, 49, 49)',
+              m: '(0, 0, 233)',
+              n: '(182, 0, 30)',
+              o: '(208, 208, 208)',
+              p: '(80, 0, 124)',
+              q: '(73, 38, 14)',
+              r: '(0, 0, 233)',
+              s: '(10, 67, 16)',
+              t: '(3, 3, 3)',
+              u: '(234, 137, 171)',
+              v: '(67, 39, 9)',
+              w: '(0, 0, 103)',
+              x: '(66, 41, 9)',
+              y: '(253, 238, 66)',
+              z: '(70, 42, 11)'
+            };
+
 var words = [
     {
         "name": "the",
@@ -31,13 +59,15 @@ function shuffle(array) {
 
  function get_input(){
   var input = $('#input').val();
-
-  //     draw(input);
-      //}
-  
-   // })  
-
+  console.log("hiiiii", input)
+    draw([{  
+      name: input,
+      length: input.length,
+      letters: input.split('')[0]
+    }]);
  }
+
+
 
 // function get_word(word){
 //   return word.name
@@ -48,11 +78,13 @@ function shuffle(array) {
 
 function project(words){
   var projection = d3.select('section').selectAll("div").data(words)
-  projection.style("font-size", function(d){
-    var length = (d.length * 2) + "px"
-    d.length = d.length * 2
-      return length
-    })
+  projection
+    .style("font-size", function(d){
+      var length = (d.length * 2) + "px"
+      if (d.length < 200) d.length = d.length * 2;
+      return length})
+    .style("color", function(d){return "rgb" + colors[d.letters]})
+        // .style("color", "rgb(23, 244, 22)")
 
 
   projection.enter()
@@ -71,7 +103,21 @@ projection
 
 
 
-function draw(){
-  project(words)
+function draw(words){
+  if (window.myInterval) clearInterval(window.myInterval);
+  window.myInterval = setInterval(function(){
+      project(words);
+  }, 500)
 }
  // window.interval = window.setInterval(draw, 600);
+
+ $(function(){
+
+  $('body').on('keypress', function(e){
+    if (e.which == 13) {
+      get_input();
+    }
+  })
+
+ })
+
